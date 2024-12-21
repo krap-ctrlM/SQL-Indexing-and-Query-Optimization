50a9a51ad1db1f3464a54d6a81c655ead1667f51
krap-ctrlM <artemmakidonskiy@gmail.com>
2024-12-21 22:21:00 +0300

    Create  RequestsFirstType.sql

---
+++ RequestsFirstType.sql
@@ -0,0 +1,21 @@
+EXPLAIN ANALYZE SELECT prep.fio, institute.name_inst FROM prep, institute WHERE prep.institute_id=institute.id_inst;
+EXPLAIN ANALYZE SELECT prep.fio FROM prep WHERE institute_id IN (SELECT id_inst FROM institute);
+
+EXPLAIN ANALYZE SELECT prep.fio, institute.name_inst FROM prep INNER JOIN institute ON prep. institute_id = institute.id_inst;
+
+
+Запросы с соединением индексированных таблиц
+Добавим кластерный индекс
+ALTER TABLE prep ADD PRIMARY KEY(id_rec);
+
+Добавим некластерные неуникальные индексы
+ALTER TABLE `prep` ADD INDEX(`institute_id`);
+ALTER TABLE `insitute` ADD INDEX(`id_inst `);
+
+
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+
+EXPLAIN ANALYZE SELECT prep.fio, institute.name_inst FROM prep, institute WHERE prep.institute_id=institute.id_inst;
+EXPLAIN ANALYZE SELECT prep.fio FROM prep WHERE institute_id IN (SELECT id_inst FROM institute);
+EXPLAIN ANALYZE SELECT prep.fio, institute.name_inst FROM prep INNER JOIN institute ON prep. institute_id = institute.id_inst;
